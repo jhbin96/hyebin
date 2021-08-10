8569d903ea13801f19bf8f0909fcf3f5faee4e27
hyebin <jhbin96@likelion.org>
2021-08-10 10:21:29 +0900

    8/10 3ban class commit 3

--- commentapp/models.py
+++ commentapp/models.py
@@ -7,9 +7,10 @@ from articleapp.models import Article
 
 class Comment(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
-                               related_name='comment',null=True)
+                               related_name='comment', null=True)
     article = models.ForeignKey(Article, on_delete=models.SET_NULL,
                                 related_name='comment', null=True)
+
     content = models.TextField(null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
